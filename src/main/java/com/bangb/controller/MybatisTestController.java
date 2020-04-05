d3a76872d87df2753667e79457869aeb9c7e9a5a
quiet <quiet_inside@126.com>
2020-04-06 00:41:59 +0800

    新增数据字典，Spring启动时候，加载自定义properties文件 并注入到Properties对象中

--- src/main/java/com/bangb/controller/MybatisTestController.java
+++ src/main/java/com/bangb/controller/MybatisTestController.java
@@ -1,11 +1,15 @@
 package com.bangb.controller;
 
+import java.util.Properties;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.codehaus.jackson.map.annotate.JsonView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,12 +28,17 @@ public class MybatisTestController {
 	@Autowired
 	private MybatisTestService testService;
 	
+	@Resource(name = "prop")
+	private Properties prop;
+	
 	
 	@RequestMapping(value="queryClassList.action",method=RequestMethod.GET)
 	@JsonView(SimpleView.class)
 	@ResponseBody
 	public String queryStudent(HttpServletRequest request) {
 		log.debug(">>>>>>>>>>>>>>>>>>>>>  spring is starting  <<<<<<<<<<<<<<<<<<<<<");
+		
+		String a = (String) prop.get("A");
 		
 		ClassDept classDept = null;
 		try {
